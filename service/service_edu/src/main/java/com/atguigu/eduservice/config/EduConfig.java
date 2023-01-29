945805be21317ade201de49ea3ed71d83083f473
tangshuaiqi <tangshuaiqi@qq.com>
2023-01-29 13:54:37 +0800

    项目简单启动 实现了简单的crud

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/config/EduConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.config;
+
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.atguigu.eduservice.mapper")
+public class EduConfig {
+
+	/**
+	 * 逻辑删除
+	 * @return
+	 */
+	@Bean
+	public ISqlInjector sqlInjector(){
+		return new LogicSqlInjector();
+	}
+}
