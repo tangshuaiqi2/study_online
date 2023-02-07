b51b9670a7a541bb1be179c8c7362bb699279339
tangshuaiqi <tangshuaiqi@qq.com>
2023-02-07 15:34:42 +0800

    使用alibaba增加了视频上传功能

--- service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -3,12 +3,14 @@ package com.atguigu.eduservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.atguigu"})
 @MapperScan(basePackages = "com.atguigu.eduservice.mapper")
+@EnableDiscoveryClient
 public class EduApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EduApplication.class, args);
